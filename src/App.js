d10daa338dcb2afe6f4afe48dafb2fa9344f5748
tony <serratedhumor@yahoo.com>
2019-11-15 12:34:46 -0600

    mvp

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import React from "react";
 import Header from "./components/Header.js";
+import CharacterList from './components/CharacterList'
+import WelcomePage from './components/WelcomePage'
+import { Route, NavLink } from 'react-router-dom'
+import SearchForm from "./components/SearchForm.js";
+import styled from 'styled-components'
 
+const StyledNav = styled.div`
+      display: flex;
+      justify-content: space-evenly
+      a{
+        text-decoration: none;
+      }
+`
 
 export default function App() {
   return (
     <main>
       <Header />
+      <StyledNav>
+        <NavLink to={'/'} >Home</NavLink>
+        <NavLink to={'/characters'} >Cast</NavLink>
+        <NavLink to={'/search'} >Search</NavLink>
+      </StyledNav>
+      <Route exact path='/' render={props => <WelcomePage {...props} />} />
+      <Route exact path='/characters' render={props => <CharacterList {...props} />} />
+      <Route exact path='/search' render={props => <SearchForm {...props} />} />
     </main>
   );
 }
--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,32 @@
-import React from "react";
+import React from 'react';
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+import styled from 'styled-components'
+
+const StyledCard = styled.div`
+    width: 30%;
+    padding: 2% 0%;
+    margin: 2%;
+    background: rgba(0,0,0,.6);
+    color: white;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    img{
+      width: 90%
+    }
+`
+
+export default function CharacterCard(props) {
+  return (
+
+    <StyledCard>
+      <h1>{props.data.name}</h1>
+      <img src={props.data.image} />
+      <h2>Species: {props.data.species}</h2>
+      <h3>Status: {props.data.status}</h3>
+
+    </StyledCard>
+  )
+
 }
